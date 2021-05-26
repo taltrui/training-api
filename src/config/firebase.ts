555177e489bd60ae01fe1fb6a4022a15e2cd389f
Tomás Altrui <tomas.altrui@widergy.com>
2021-05-26 11:27:00 -0300

    fix: moved service account to env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,17 +1,26 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/firestore"
-import fbAdmin, {ServiceAccount} from 'firebase-admin';
+import "firebase/firestore";
+import fbAdmin, { ServiceAccount } from "firebase-admin";
 
 dotenv.config();
 
-import serviceAccount  from "../../serviceAccountKey.json";
-
+const serviceAccount = {
+  type: "service_account",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+  private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+  client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+  client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+  client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+};
 export const admin = fbAdmin.initializeApp({
-  credential: fbAdmin.credential.cert(serviceAccount as ServiceAccount)
+  credential: fbAdmin.credential.cert(serviceAccount as ServiceAccount),
 });
-
 
 const firebaseConfig = {
   apiKey: process.env.FIRESTORE_API_KEY,
@@ -21,7 +30,7 @@ const firebaseConfig = {
   storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIRESTORE_MESSAGING_SENDER_ID,
   appId: process.env.FIRESTORE_APP_ID,
-  measurementId: process.env.FIRESTORE_MEASUREMENT_ID
+  measurementId: process.env.FIRESTORE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
