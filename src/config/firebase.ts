47c58ac36725e526b2711ba6e5423ab6420d38ab
Tomás Altrui <tomas.altrui@widergy.com>
2021-05-26 11:40:04 -0300

    fix: key fix

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -13,7 +13,7 @@ const serviceAccount = {
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
   private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
+  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
   client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
   client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
